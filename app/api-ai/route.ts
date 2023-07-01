c70458564a9e5eef4fb50ef3b8deaf6d46fbfd23
tfutada <tafutada@gmail.com>
2023-07-01 12:22:25 +0900

    init

--- app/api-ai/route.ts
+++ app/api-ai/route.ts
@@ -1,5 +1,5 @@
-import { OpenAIStream, StreamingTextResponse } from 'ai'
-import { Configuration, OpenAIApi } from 'openai-edge'
+import {OpenAIStream, StreamingTextResponse} from 'ai'
+import {Configuration, OpenAIApi} from 'openai-edge'
 
 // Create an OpenAI API client (that's edge friendly!)
 const config = new Configuration({
@@ -12,16 +12,26 @@ export const runtime = 'edge'
 
 export async function POST(req: Request) {
     // Extract the `messages` from the body of the request
-    const { messages } = await req.json()
+    const {messages} = await req.json()
 
-    // Ask OpenAI for a streaming chat completion given the prompt
-    const response = await openai.createChatCompletion({
-        model: 'gpt-3.5-turbo',
-        stream: true,
-        messages
-    })
-    // Convert the response into a friendly text-stream
-    const stream = OpenAIStream(response)
-    // Respond with the stream
-    return new StreamingTextResponse(stream)
+    console.log(messages)
+
+    try {
+        const response = await openai.createChatCompletion({
+            model: 'gpt-3.5-turbo',
+            stream: true,
+            messages
+        })
+        console.log('done')
+        // Convert the response into a friendly text-stream
+        const stream = OpenAIStream(response)
+        // Respond with the stream
+        return new StreamingTextResponse(stream)
+
+    } catch (error) {
+        console.error('An error occurred while creating the chat completion:', error)
+        // Handle the error appropriately in your application
+        // For example, you might want to send a response with an error message:
+        return new Response(JSON.stringify({error: 'An error occurred while creating the chat completion'}), {status: 500})
+    }
 }
