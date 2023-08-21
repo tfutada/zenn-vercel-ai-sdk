b011f788195ac1e6e5cebc3f82498a8bcac22c6f
tfutada <tafutada@gmail.com>
2023-08-21 13:36:21 +0900

    use openai

--- app/chat/client.tsx
+++ app/chat/client.tsx
@@ -4,7 +4,7 @@ import {useChat} from 'ai/react'
 
 export default function Chat() {
     // change the index to switch between the different APIs. ex. 0, 1, 2, 3
-    const api = ["/api-openai", "/api-huggingface", "/api-anthropic", "/api-replicate"][3]
+    const api = ["/api-openai", "/api-huggingface", "/api-anthropic", "/api-replicate"][0]
 
     const {messages, input, handleInputChange, handleSubmit} = useChat({api})
 
